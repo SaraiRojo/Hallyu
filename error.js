666d49462978f7b1d9da8bf5663982b471edfeb5
SaraiRojo <103267214+SaraiRojo@users.noreply.github.com>
2023-10-23 21:57:35 -0600

    Delete error.js

--- error.js
+++
@@ -1,4 +0,0 @@
-function regresarInicio() {
-    // Redirige al usuario a la página de inicio o la página principal de tu sitio.
-    window.location.href = "index.html"; // Cambia "index.html" por tu página de inicio.
-}
