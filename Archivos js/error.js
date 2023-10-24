30dd5888a86c9837a4a7acede359ebec5c6d8ae3
SaraiRojo <103267214+SaraiRojo@users.noreply.github.com>
2023-10-23 22:00:22 -0600

    Update error.js

--- Archivos js/error.js
+++ Archivos js/error.js
@@ -1,4 +1,4 @@
 function regresarInicio() {
-    // Redirige al usuario a la página de inicio o la página principal de tu sitio.
+    // Redirige al usuario a la página de inicio o la página principal de tu sitio. 
     window.location.href = "index.html"; // Cambia "index.html" por tu página de inicio.
 }
